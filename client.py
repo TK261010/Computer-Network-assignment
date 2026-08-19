531ebac5cf3f3d50acb353130799b961c54a08c6
TK261010 <tanmaykadam2223@ternaengg.ac.in>
2026-08-19 19:43:23 +0530

    Remove unnecessary URL from client.py

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 5000
+https://github.com/TK261010/Computer-Network-assignment/tree/main
+# Create socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to server
+client_socket.connect((HOST, PORT))
+
+print("===================================")
+print("          CLIENT PROGRAM")
+print("===================================")
+
+# Take message from user
+message = input("Enter message to send to server: ")
+
+# Send message
+client_socket.send(message.encode())
+
+print("Message sent to server.")
+
+# Receive response
+response = client_socket.recv(1024).decode()
+
+print("Response from server:", response)
+
+# Close connection
+client_socket.close()
+
+print("Client connection closed.")
